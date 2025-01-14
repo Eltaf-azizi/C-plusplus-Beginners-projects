f22d5cf71bf745530634456f60b370ada5e5b860
Eltaf-azizi <iltfhuss@gmail.com>
2025-01-14 21:34:15 +0500

    added the switch statement and class

---
+++ login.cpp
@@ -0,0 +1,39 @@
+#include<iosream>
+#include<fstream>
+using namespace std;
+
+
+class temp{
+    
+    string userName, Email, password;
+}obj;
+
+
+
+int main() [
+    char choice;
+    cout <<"\1- Login";
+    cout <<"\2- Sign-Up";
+    cout <<"\3- Forgot Password";
+    cout <<"\4- Exit";
+    cout <<"\nEnter Your Choice :: ";
+    cin>> choice;
+
+    switch(choice){
+
+        case '1':
+        break;
+
+        case '2':
+        break;
+
+        case '3':
+        break;
+
+        case '4':
+        break;
+
+        default:
+        cout << "Invalid selection...!";
+    }
+]
