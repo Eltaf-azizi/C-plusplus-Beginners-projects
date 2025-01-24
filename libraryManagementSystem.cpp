c62c205e5fc0ed249ef4223f341281bef3ac0f2a
Eltaf-azizi <iltfhuss@gmail.com>
2025-01-24 22:36:50 +0500

    done! the library management system project

--- libraryManagementSystem.cpp
+++ libraryManagementSystem.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 class temp{
 
-    string id, name, author;
+    string id, name, author, search;
     fstream file;
     public:
     void addBook();
     void showAll();
     void extractBook();
 
-}
+}obj;
 
 int main(){
 
@@ -28,15 +28,22 @@ int main(){
 
     switch(choice){
         case '1':
+            cin.ignore();
+            obj.showAll();
         break;
 
         case '2':
+            cin.ignore();
+            obj.extractBook();
         break;
 
         case '3':
+            cin.ignore();
+            obj.addBook();
         break;
 
         case '4':
+        return 0;
         break;     
 
         default:
@@ -84,8 +91,28 @@ void temp :: showAll(){
 
 void temp :: extractBook(){
 
+    showAll();
+    cout << "Enter Book ID :: ";
+    getline(cin, search);
+
     file.open("bookData.txt", ios :: in);
     getline(file, id, '*');
     getline(file, name, '*');
     getline(file, author, '\n');
+
+    cout << "\n\n";
+    cout << "\t\t Book Id \t\t\t Book Name \t\t\t Author's Name" << endl;
+
+    while(!file.eof()) {
+
+        if(search == id) {
+            cout << "\t\t "<<id<<" \t\t\t "<<name<<" \t\t\t "<<author<<endl;
+            cout << "Book Extracted Successfully...!";
+        }
+
+    getline(file, id, '*');
+    getline(file, name, '*');
+    getline(file, author, '\n');
+    }
+    file.close();
 }
