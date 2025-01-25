91b457f27ebbdd8723639cd396de88d44001a468
Eltaf-azizi <iltfhuss@gmail.com>
2025-01-25 22:42:00 +0500

    working on the main function

---
+++ Student ManagementSystem.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+int main() {
+    char choice;
+    cout << "------------------------------------";
+    cout << " 1- Add Student Record";
+    cout << "2- View All Student Record";
+    cout << "3- Search Student Record";
+    cout << "4- Exit";
+    cout << "------------------------------------";
+    cin>> choice;
+
+    switch(choice){
+        
+        case '1':
+        break;
+
+        case '2':
+        break;
+
+        case '3';
+        break;
+
+        case '4';
+        break;
+
+        default:
+        cout << "Invalid choice...!";
+    }
+}
