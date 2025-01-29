e894fa33e176471b80a42d4dbf7cd163cadd334f
Eltaf-azizi <iltfhuss@gmail.com>
2025-01-29 22:23:54 +0500

    working on switch statement

--- generalStoreManagementSystem.cpp
+++ generalStoreManagementSystem.cpp
@@ -7,6 +7,22 @@ int main() {
     
     cout << "press 1 --> Start Shopping";
     cout << "press 0 --> Exit";
+    cin>>choice;
+
+    switch(choice) {
+        case 'A':
+        break;
+
+        case 1:
+        break;
+
+        case 0:
+        break;
+
+        default:
+            cout << "Invalid Selection...!";
+        break;
+    }
 
     return 0;
 }
