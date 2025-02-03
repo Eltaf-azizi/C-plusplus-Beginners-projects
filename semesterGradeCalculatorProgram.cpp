a6ab77e693b3577f5aeb2840aaad35b71f75129d
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-03 22:48:22 +0500

    working on findGrade function

--- semesterGradeCalculatorProgram.cpp
+++ semesterGradeCalculatorProgram.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
 using namespace std;
 
+
+class findGrade(
+    float q1, q2, qT, midMarks, finalMarks;
+    float percentage, percQ, percM, percF;
+
+    public:
+    void getMarks();
+    void calcMarks();
+    void calcGrade();
+    void showGrade();
+)
 int main() {
 
 
     return 0;
+}
+
+void finalGrade :: getMarks() {
+    cout<<"Enter obtained marks of Quiz # out of (10) :: ";
+    cin>>q1;
+    cout<<"Enter obtained marks of Quiz # out of (10) :: ";
+    cin>>q2;
+    cout<<"Enter obtained marks of Mids term out of (100) :: ";
+    cin>>midMarks;
+    cout<<"Enter obtaned marks of final term out of (100) :: ";
+    cin>>finalMarks;
 }
