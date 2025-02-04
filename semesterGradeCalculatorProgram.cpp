51e587911a3ea7a163b6c20e6010be8d3c6ad77a
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-04 23:25:58 +0500

    working on findGrade function

--- semesterGradeCalculatorProgram.cpp
+++ semesterGradeCalculatorProgram.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class findGrade(
     float q1, q2, qT, midMarks, finalMarks;
     float percentage, percQ, percM, percF;
+    string grade;
 
     public:
     void getMarks();
@@ -19,7 +20,7 @@ int main() {
 }
 
 void finalGrade :: getMarks() {
-    cout<<"Enter obtained marks of Quiz # out of (10) :: ";
+    cout<<"\nEnter obtained marks of Quiz # out of (10) :: ";
     cin>>q1;
     cout<<"Enter obtained marks of Quiz # out of (10) :: ";
     cin>>q2;
@@ -27,4 +28,33 @@ void finalGrade :: getMarks() {
     cin>>midMarks;
     cout<<"Enter obtaned marks of final term out of (100) :: ";
     cin>>finalMarks;
+    cout<<endl;
+}
+
+
+void findGrade :: calcMarks() {
+    qT = q1 + q2;
+    percQ = (23.0 / 20.0) * qT;
+    percM = (25.0 / 100.0) * midMarks;
+    percF = (50.0 / 100.0) * finlaMarks;
+    percentage = percQ + percM + percF;
+}
+
+
+void findGrade :: calcGrade() {
+    if(percentage >= 90.0) 
+        grade = "A";
+
+    if(percentage >= 80.0 && percentage < 90.0)
+        grade = "B";
+
+    if(percentage >= 70.0 && percentage < 80.0)
+        grade = "C";
+
+    if(percentage >= 60.0 && percentage < 70.0)
+        grade = "D";
+
+    if(percentage >= 50.0 && percentage < 60.0)
+        grade = "E";
+    
 }
