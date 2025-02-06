82d1030a7c4d4e9cfe08bd728fee4e2bd6005435
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-06 23:25:02 +0500

    working on addContact function

--- contactManagementSystem.cpp
+++ contactManagementSystem.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<fstream>
 using manespace std;
 
 class temp{
 
+    string phoneNo, name, address, description;
+    fstream file;
     public:
     void addContact();
     void showAll();
@@ -13,4 +16,19 @@ int main() {
 
 
     return 0;
+}
+
+
+void temp :: addContact(){
+    cout << "Enter phone Number :: ";
+    getline(cin, phoneNo);
+    cout <<"Enter Name :: ";
+    getline(cin, name);
+    cout << "Enter Address :: ";
+    getline(cin, address);
+    cout << "Enter Description :: ";
+    getline(cin, description);
+
+    file.open("data.csv", ios :: out | ios :: app);
+    
 }
