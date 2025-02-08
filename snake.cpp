e7386f5846f1fc58353c8c6ea65958b209539f14
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-08 23:23:05 +0500

    begin working on functions

--- snake.cpp
+++ snake.cpp
@@ -4,6 +4,20 @@
 uisng namespace std;
 
 
+enum Direction{STOP = 0, LEFT, RIGHT, UP, DOWN};
+Direction dir;
+bool gameOver;
+const int height = 20;
+const int width = 20;
+int headX, headY, fruitX, fruitY, score;
+int tail_len;
+
+
+void setup();
+void draw();
+void input();
+void logic();
+
 int main() {
     char start;
     cout<< "\t------------------------------------" << endl;
@@ -13,7 +27,26 @@ int main() {
     cin >> start;
 
     if(start == 's') {
-        cout << "\tGame Start";
+        setup();
     }
     return 0;
+}
+
+void setup(){
+
+}
+
+
+void draw(){
+
+}
+
+
+void input(){
+
+}
+
+
+void logic(){
+    
 }
