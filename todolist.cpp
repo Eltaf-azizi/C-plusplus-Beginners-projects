47e2436d6128030129dfc2441809505d9c3ceca1
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-11 20:56:30 +0500

    working on banner

--- todolist.cpp
+++ todolist.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void banner();
 
 int main() {
     
+    system("cls");
+    while(true){
+
+        banner();
+        cout << "\n\t1. Add Task";
+        cout << "\n\t2. Show Task";
+        cout << "\n\t3. Search Task";
+        cout << "\n\t4. Delete Task";
+        cout << "\n\t5. Update Task";
+
+        int choice;
+        cout << "\n\tEnter Choice: ";
+        cin >> choice;
+    }
 
     return 0;
 }
