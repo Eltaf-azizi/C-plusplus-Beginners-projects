e4ce858c43665a490c18819da7c2b0e6aa62e5c4
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-16 22:52:30 +0500

    define the variables

--- calculator.cpp
+++ calculator.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main() {
 
+    // define variables
+    string operation;
+    double number1 = 2, number2 = 3;
+
+    // get user input
+    cout << "Enter First Number: ";
+    cin >> number1;
+
+    cout << "Enter Operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter Second Number: ";
+    cin >> number2;
+
+    // if else statement checking entered operation
 
     return 0;
 }
