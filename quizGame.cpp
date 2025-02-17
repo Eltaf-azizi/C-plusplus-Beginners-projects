1153763999ea2425852468bdf86fe0136888af2d
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-17 21:23:57 +0500

    start creating on quiz game

---
+++ quizGame.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    string user_input;
+    cout << "================ Welcome! =================";
+    cout << "If you to play game then enter 'play' to start: ";
+
+    cin >> user_input;
+
+
+    return 0;
+}
