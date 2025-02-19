bf244e2861d72b2765a5f6652117e79e393e9a31
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-19 22:17:19 +0500

    working on the conditions

--- clock.cpp
+++ clock.cpp
@@ -8,6 +8,21 @@ int main(){
     int h, m, s, a, err;
     err = a = 0;
 
+    while (err==0){
+        cout << "enter hour: " << endl;
+        cin >> h;
+        cout << "enter minutes: " << endl;
+        cin >> m;
+        cout << "enter seconds: " << endl;
+        cin >> s;
+
+        if (h  <24 && m < 60 && s < 60){
+            err++;
+        }
+        else{
+            system("cls");
+        }
+    }
 
     return 0;
 
