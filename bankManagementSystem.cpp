91d06f095b78be71442c5d4fdf644d0d898bef4a
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-20 21:50:32 +0500

    start working on bank management system

---
+++ bankManagementSystem.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream.h>
+
+
+int main() 
+{
+    cout << "1) Open account \n";
+    cout << "2) Deposit Money \n";
+    cout << "3) Withdraw Money \n";
+    cout << "4) Display Account \n";
+    cout << "5) Exit";
+
+
+    getch();
+    return 0;
+}
