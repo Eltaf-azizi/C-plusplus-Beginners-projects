6c5a89b253169e3cf2ae27275addfaff70d5a808
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-22 23:46:17 +0500

    worked on do loop

--- bankManagementSystem.cpp
+++ bankManagementSystem.cpp
@@ -61,7 +61,7 @@ void bank :: withdraw_money()
 
 int main() 
 {
-    int ch;
+    int ch, x;
     bank obj;
     do{
     cout << "1) Open account \n";
@@ -75,19 +75,43 @@ int main()
 
     switch(ch)
     {
-        case 1: << "1) Open account \n";
+        case 1: 
+        cout << "1) Open account \n";
         obj.open_account();
         break;
 
-        case 2: << "2) Deposit Money \n";
+        case 2: 
+        cout << "2) Deposit Money \n";
         obj.deposit_money();
         break;
 
-        case 3: << "3) Withdraw Money : \n";
+        case 3:
+        cout << "3) Withdraw Money : \n";
         obj.withdraw_money();
         break;
 
+        case 4:
+        cout << "4) Display Account \n";
+        obj.display_account();
+        break;
+        case 5:
+        if(ch==5)
+        {
+            exit(1);
+        }
+        default:
+        cout << "This is not exist try again \n";
 
+
+    }
+    cout << "\n Do you want to select next option then press :: y";
+    cout << "\n Do you want to select next option then press :: y \n";
+    cout << "If you want to exit then press :: N ";
+    x=getch();
+
+    if(x=='n' || x=='N') 
+    {
+        exit(0);
     }
     }
 
