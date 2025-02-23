53cf1ec57a90778ec7f293e7a0f74cdca5dfba68
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-23 23:10:11 +0500

    working on the info function

--- hospitalManagementSystem.cpp
+++ hospitalManagementSystem.cpp
@@ -16,4 +16,27 @@ void cpasscode();
 class one 
 {
     public;
+    virtual void get()=0;
+    virtual void sow()=0;
+}
+
+
+class inof:public one
+{
+    public;
+    char name[50], time[50];
+    int sum, age;
+    void get()
+    {
+        system("cls");
+        cin.sync();
+        cout << "\n Enter the patient name =";
+        cin.getline(name, 50);
+        cout << "\n Enter the appointment time =";
+        cin.getline(time, 50);
+        cout << "\n Enter age =";
+        cin>>age;
+        cout << "\n Enter appointment number =";
+        cin>>num;
+    }
 }
