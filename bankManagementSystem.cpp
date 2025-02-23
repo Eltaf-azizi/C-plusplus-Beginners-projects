1a34c414ac4c33c9423964afe0c94dbd7aa81c41
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-23 21:36:23 +0500

    start working on hospital management system

--- bankManagementSystem.cpp
+++ bankManagementSystem.cpp
@@ -64,57 +64,59 @@ int main()
     int ch, x;
     bank obj;
     do{
-    cout << "1) Open account \n";
-    cout << "2) Deposit Money \n";
-    cout << "3) Withdraw Money \n";
-    cout << "4) Display Account \n";
-    cout << "5) Exit";
-    cout << "Select the option from above \n";
-    cin >> ch;
-
-
-    switch(ch)
-    {
-        case 1: 
         cout << "1) Open account \n";
-        obj.open_account();
-        break;
-
-        case 2: 
         cout << "2) Deposit Money \n";
-        obj.deposit_money();
-        break;
-
-        case 3:
-        cout << "3) Withdraw Money : \n";
-        obj.withdraw_money();
-        break;
-
-        case 4:
+        cout << "3) Withdraw Money \n";
         cout << "4) Display Account \n";
-        obj.display_account();
-        break;
-        case 5:
-        if(ch==5)
+        cout << "5) Exit";
+        cout << "Select the option from above \n";
+        cin >> ch;
+
+
+        switch(ch)
         {
-            exit(1);
+            case 1: 
+            cout << "1) Open account \n";
+            obj.open_account();
+            break;
+
+            case 2: 
+            cout << "2) Deposit Money \n";
+            obj.deposit_money();
+            break;
+
+            case 3:
+            cout << "3) Withdraw Money : \n";
+            obj.withdraw_money();
+            break;
+
+            case 4:
+            cout << "4) Display Account \n";
+            obj.display_account();
+            break;
+            case 5:
+            if(ch==5)
+            {
+                exit(1);
+            }
+            default:
+            cout << "This is not exist try again \n";
+
+
         }
-        default:
-        cout << "This is not exist try again \n";
+        cout << "\n Do you want to select next option then press :: y";
+        cout << "\n Do you want to select next option then press :: y \n";
+        cout << "If you want to exit then press :: N ";
+        x=getch();
 
-
+        
+        if(x=='n' || x=='N') 
+        {
+            exit(0);
+        }
     }
-    cout << "\n Do you want to select next option then press :: y";
-    cout << "\n Do you want to select next option then press :: y \n";
-    cout << "If you want to exit then press :: N ";
-    x=getch();
-
-    if(x=='n' || x=='N') 
-    {
-        exit(0);
-    }
-    }
-
+    while(x=='y' || x='Y');
+ 
     getch();
     return 0;
 }
---
+++ hospitalManagementSystem.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<iomanip>
+#include<cstring>
+#include<windows.h>
+using namespace std;
+
+
+
+void menu();
+void passcode();
+void cpasscode();
+
+
+class one 
+{
+    public;
+}
