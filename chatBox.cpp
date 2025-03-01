91f9a8db67734f58a219f739a6631694c25d6252
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-01 23:37:06 +0500

    woking on the conditions

--- chatBox.cpp
+++ chatBox.cpp
@@ -1,13 +1,43 @@
 #include<windows.h>
-#include<iostream>
-#include<string>
-
-
+#include <iostream>
+#include <string>
 using namespace std;
+
+
 
 int main()
 {
+    char inp, y, n, Y, N;
+    string name, getKey;
 
+    cout << "\t\t WELCOME TO CHATBOX\n";
+    cout << "Do you want to chat with me? (Y/N)\n";
+    cout << "--> ";
+    cin>>inp;
+
+
+    //condition (y/n)
+    if(inp == 'y' || inp == 'Y')
+    {
+        cout << "\n Welcome...";
+    }
+    else
+    {
+        cout << "\n Thanks for coming...";    
+
+        return 0;
+    }
+    system("cls");
+
+
+    if(getKey != "bi")
+    {
+        cout << "\t HELLO USER \n";
+        cin>>getKey;
+        cout << "What is your name? \n";
+        cin>>name;
+        cout << name << "How old are you? \n";
+    }
 
     return 0;
-}
+};
