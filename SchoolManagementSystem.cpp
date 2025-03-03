e07dfdc28bcc28e0c96b544f517e94f0fa542374
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-03 19:03:21 +0500

    write the neccessary thing

--- SchoolManagementSystem.cpp
+++ SchoolManagementSystem.cpp
@@ -7,6 +7,31 @@
 using namespace std;
 
 
+struct student
+{
+    string fname;
+    string lname;
+    string registration;
+    string classes;
+}studentData;
+
+
+
+struct teacher
+{
+    string fst_name;
+    string lst_name;
+    string qualification;
+    string exp;
+    on
+    string pay;
+    string subj;
+    string lec;
+    string addrs;
+    string cel_no;
+    string blod_grp;
+    string serves;
+}tech[50];
 
 int main()
 {
