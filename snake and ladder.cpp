cac57082d1bf941b0de82da1f085a23e58bf8d4b
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-08 19:53:11 +0500

    start working on the snake and ladder game

---
+++ snake and ladder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    int ladder[] = {5, 20, 35, 50, 65, 80, 92};
+    int snake[] = {15, 25, 37, 54, 74, 34, 45};
+    int board = 100;
+}
