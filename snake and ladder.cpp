910fdead2040f4b5a614980ac9f5ed26f7ae5f69
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-09 16:26:17 +0500

    working on the gameOver function

--- snake and ladder.cpp
+++ snake and ladder.cpp
@@ -12,4 +12,23 @@ int main()
     int ladder[] = {5, 20, 35, 50, 65, 80, 92};
     int snake[] = {15, 25, 37, 54, 74, 34, 45};
     int board = 100;
+    int maxPlayer = 2;
+    bool gameOver = false;
+    int current[maxPlayer] = {0};
+    int player = 0;
+
+
+
+    while(!gameOver)
+    {
+        cout << "Snake and Ladder game!";
+        cout << "Current Player " player+1 << endl;
+        cout << "=========================" << endl;
+
+
+        for(iint =0; i<maxPlayer; i++)
+        {
+
+        }
+    }
 }
