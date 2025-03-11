c0d37dc9c1a98997804f4f6429c7613c6d5db780
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-11 19:27:21 +0500

    start working on the casino number guessing

---
+++ casino number guessing.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main() 
+{
+    string playerName;
+    int amount;
+
+
+    return 0;
+}
