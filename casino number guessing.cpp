39ae4281bfb9bb529cc20aa549e3ad123af9f1bf
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-11 19:33:17 +0500

    working on do while loop and add simple things

--- casino number guessing.cpp
+++ casino number guessing.cpp
@@ -11,7 +11,25 @@ int main()
 {
     string playerName;
     int amount;
+    int bettingAmount;
+    int guess;
+    int dice;
+    char choice;
 
+    srand(time(0));
 
-    return 0;
+    drawLine(60, '_');
+    cout << "\n\n\n\t\t CASINO GAME \n\n\n\n";
+    drawLine(60, '_');
+
+    cout << "\n\n Enter Your Name: ";
+    getline(cin, playerName);
+
+    cout << "\n\n Enter deposit amount to play : $";
+
+    do
+    {
+
+    }
+    while(choice == 'Y' || choice == 'y');
 }
