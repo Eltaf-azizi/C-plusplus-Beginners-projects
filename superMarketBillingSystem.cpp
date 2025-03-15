430dbc802d9a6cd4896c37b514adcbf68a17bb32
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-15 15:35:52 +0500

    added neccessary things that need in this project

--- superMarketBillingSystem.cpp
+++ superMarketBillingSystem.cpp
@@ -14,4 +14,35 @@ class Bill
 
     public:
     Bill():Item(""), Rate(0), Quantity(0){ }
+
+    void setItem(string item)
+    {
+        Item = item;
+    }
+
+    void setRate(int rate)
+    {
+        Rate = rate;
+    }
+
+    void setQuant(int quant)
+    {
+        Quantity = quant;
+    }
+
+
+    string getItem()
+    {
+        return Item;
+    }
+
+    int getRate()
+    {
+        return Rate;
+    }
+
+    int getQuant()
+    {
+        return Quantity;
+    }
 };
