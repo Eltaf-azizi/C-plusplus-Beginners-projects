72d75fadbe6de4ae39606b4acdc449f17eaf8bf9
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-17 16:26:34 +0500

    adding neccessary things that needed

--- hostelAccommodationSystem.cpp
+++ hostelAccommodationSystem.cpp
@@ -11,4 +11,36 @@ class Hostel
     private:
     string Name;
     int Rent, Bed;
+
+    public:
+    Hostel(string name, int rent, int bed)
+    {
+        Name = name;
+        Rent = rent;
+        Bed = bed;
+    }
+
+
+    string getName()
+    {
+        return Name;
+    }
+
+
+    int getRent()
+    {
+        return Rent;
+    }
+
+
+    int getBed()
+    {
+        return Bed;
+    }
+};
+
+
+class Student
+{
+    
 }
