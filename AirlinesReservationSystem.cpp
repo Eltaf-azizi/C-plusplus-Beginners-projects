eba012f3937d1b1295c7e1e0c2a98baaa75b0cdc
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-18 21:49:27 +0500

    adding the neccessary things that needed in this project

--- AirlinesReservationSystem.cpp
+++ AirlinesReservationSystem.cpp
@@ -5,9 +5,39 @@
 
 using namespace std;
 
-
-int main()
+class Airline
 {
+    private:
+    string Flight, Des, Dep;
+    int Seats;
 
-    return 0;
-}
+    public:
+    Airline(string flight, string des, string dep, int seats)
+    {
+        Flight = flight;
+        Des = des;
+        Dep = dep;
+        Seats = seats;
+    }
+
+
+
+    string getFlight()
+    {
+        return Flight;
+    }
+
+
+
+    string getDep()
+    {
+        return Dep;
+    }
+    
+
+
+    int getSeat()
+    {
+        return Seats;
+    }
+};
