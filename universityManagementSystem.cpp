4403a8e559a0f0542a55cd4010ba7edefc3095c9
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-19 20:50:12 +0500

    start working on the university management system

---
+++ universityManagementSystem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+
+class University
+{
+    private:
+    string RollNo, Name, Subject, Address;
+
+    public:
+    University():RollNo(""), Name(""), Subject(""), Address(""){}
+    
+}
