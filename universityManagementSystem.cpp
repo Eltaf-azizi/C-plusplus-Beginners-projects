2e32cf2d0709d0fc6a6f2b1b59262c9f0b2c9b7e
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-20 15:48:06 +0500

    working to add student

--- universityManagementSystem.cpp
+++ universityManagementSystem.cpp
@@ -63,6 +63,11 @@ class University
 };
 
 
+add(University student)
+{
+    
+}
+
 
 int main()
 {
@@ -72,5 +77,22 @@ int main()
     while(!exit)
     {
         system("cls");
+        int val;
+
+        cout << "\t Welcome to University Management System" <<endl;
+        cout << "\t ***************************************" <<endl;
+        cout << "\t 1. Add Student." <<endl;
+        cout << "\t 2. Search Student." <<endl;
+        cout << "\t 3. Update Data Of Student." <<endl;
+        cout << "\t 4. exit." <<endl;
+        cout << "\t Enter Your Choice: ";
+        cin val;
+
+
+        if(val == 1)
+        {
+            system("cls");
+            Sleep(6000);
+        }
     }
 }
