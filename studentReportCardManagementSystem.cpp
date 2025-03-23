49e26bafe622cb9771cd4d50376b45bf852a5a56
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-23 21:28:03 +0500

    start working on Student Report Card Management System

---
+++ studentReportCardManagementSystem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+
+class Student
+{
+    private:
+    string RollNo, Name, Grade;
+
+    public:
+    Student(): RollNo(""), Name("")
+};
