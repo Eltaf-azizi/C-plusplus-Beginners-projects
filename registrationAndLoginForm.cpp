d6de3b7072d4452a9e8ef719ef8403124fefd1ca
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-28 20:24:29 +0500

    adding the neccessary things

--- registrationAndLoginForm.cpp
+++ registrationAndLoginForm.cpp
@@ -6,6 +6,23 @@
 using namespace std;
 
 
+class Login
+{
+    private:
+    string LoginID, Password;
+
+    public:
+    Login():LoginID(""), Password(""){}
+
+
+
+    void setID(string id)
+    {
+        LoginID = id;
+    }
+}
+
+
 int main()
 {
     
