2fd75662f46c1986208b77350c192b47a6ba0eb4
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-02 20:18:23 +0000

    adding neccessary things needed in this project

--- superMarketManagementSystem.cpp
+++ superMarketManagementSystem.cpp
@@ -11,4 +11,26 @@ class Market
     private:
     string name;
     int Quantity, Price;
-}
+    public:
+    Market(): Name(""), Quantity(0), Price(0){}
+
+
+
+    setName(string name)
+    {
+        Name = name;
+    }
+
+
+    setQuant(int quantity)
+    {
+        Quantity = quantity;
+    }
+
+
+
+    string getName()
+    {
+        return Name;
+    }
+};
