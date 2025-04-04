aa577bd8f2ef2fe375b805d706585ae5c61d07b4
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-04 20:32:06 +0500

    working on the main function

--- movieTicketBookingSystem.cpp
+++ movieTicketBookingSystem.cpp
@@ -6,9 +6,25 @@
 using namespace std;
 
 
+const char* HOST = 'localhost';
+const char* USER = "root";
+const char* PW = "your password";
+const char* DB = "mydb";
+
 
 int main()
 {
+    MYSQL * conn;
+    conn = mysql_init(NULL);
+
+    if(!mysql_real_connect(conn, HOST, USER, PW, DB, 3306, NULL, 0))
+    {
+        cout << "Error: " << mysql_error(conn);
+    }
+    else
+    {
+        
+    }
 
     return 0;
 }
