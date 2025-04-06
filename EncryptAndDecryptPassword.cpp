4955b31209e4ebe56f9e0c840893211b853c3078
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-06 20:06:58 +0500

    adding the neccessary things

--- EncryptAndDecryptPassword.cpp
+++ EncryptAndDecryptPassword.cpp
@@ -6,6 +6,21 @@
 using namespace std;
 
 
+const char* HOST = "localhost";
+const char* USER = "root";
+const char* PW = " ";
+const char* DB = "mydb";
+
+
+class Login
+{
+    private:
+    string userId, userPW;
+    public:
+    Login(): userId(""), userPW("") {}
+};
+
+
 
 int main()
 {
