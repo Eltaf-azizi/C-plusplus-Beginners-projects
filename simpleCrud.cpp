7548cd34c90c7e79db8739d9fe9f51e21b5f869d
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-08 20:49:01 +0500

    working on the main function

--- simpleCrud.cpp
+++ simpleCrud.cpp
@@ -6,9 +6,27 @@
 using namespace std;
 
 
+const char * HOST = "localhost";
+const char * USER = "root";
+const char * PW = "password";
+const char * DB = "mydb";
+
 
 int main()
 {
+    MYSQL * conn;
+    conn = mysql_init(NULL);
+
+    if(!mysql_real_connect(conn, HOST, USER, PW, 3306, NULL, 0))
+    {
+        cout <<endl <<endl;
+        cout << "\t\t Error: " <<mysql_error(conn) <<endl;
+    }
+    else
+    {
+        cout <<endl <<endl;
+        cout << "\t\t Logged in" <<endl;
+    }
 
     return 0;
 }
