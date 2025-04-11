e8cf2acf91bcbcfa159b3e9d5ec3a3d44f585935
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-11 20:21:19 +0500

    adding the neccessary things

--- carRentalSystem.cpp
+++ carRentalSystem.cpp
@@ -7,6 +7,26 @@
 using namespace std;
 
 
+const char* HOST = "localhost";
+const char* USER = "root";
+const char* PW = " ";
+const char* DB = "mydb";
+
+
+class Car
+{
+    private:
+    string Brand, Model;
+    int Serial, Rent;
+    bool Avail;
+
+    public:
+    Car(int serial, string brand, string model, int rent, bool avail)
+    {
+        
+    }
+};
+
 
 int main()
 {
