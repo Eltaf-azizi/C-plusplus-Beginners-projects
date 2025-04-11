3f67631ea501288f0acb4ca9de73732f03ffc08b
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-11 21:17:33 +0500

    adding the neccessary things

--- carRentalSystem.cpp
+++ carRentalSystem.cpp
@@ -71,8 +71,22 @@ int main()
 
     if(!mysql_real_connect(conn, HOST, USER, PW, DB, 3306, NULL, 0))
     {
-        
+        cout << "Error: " mysql_error(conn) <<endl;
     }
+    else
+    {
+        cout << "Logged in!" <<endl;
+    }
+
+    Car car1(123, "Honda", "Civic", 60, true);
+    Car car2(223, "Toyota", "Yaris", 50, true);
+    Car car3(323, "Suzuki", "Alto", 30, true);
+
+    string save1 = "INSERT INTO cars (Serial, Brand, Model, Rent, Avail) VALUES(123, 'Honda', 'Civic', 60, true)";
+    string save2 = "INSERT INTO cars (Serial, Brand, Model, Rent, Avail) VALUES(223, 'Toyota', 'Yaris', 50, true)";
+    string save3 = "INSERT INTO cars (Serial, Brand, Model, Rent, Avail) VALUES(323, 'Suzuki', 'Alto', 30, true)";
+
+    mysql_close(conn);
 
     return 0;
 }
